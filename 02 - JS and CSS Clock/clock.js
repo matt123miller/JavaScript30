20d1434cc232ab1e259a6c7566e07e84bb67f551
Matt Miller <matt123miller@hotmail.co.uk>
2018-05-05 19:29:12 +0100

    Added minute and hour hands. Maybe I could add numbers to the clockface?

--- 02 - JS and CSS Clock/clock.js
+++ 02 - JS and CSS Clock/clock.js
@@ -4,9 +4,15 @@ const minuteHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
-    
+
     const now = new Date();
-    const secondsDeg = (360 / 60) * now.getSeconds();
+    const secondsDeg = (360 / 60) * now.getSeconds() + 90;
+    const minutesDeg = (360 / 60) * now.getMinutes() + 90;
+    const hoursDeg = (360 / 12) * now.getHours() + 90;
+
     secondHand.style.transform = `rotate(${secondsDeg}deg)`;
+    minuteHand.style.transform = `rotate(${minutesDeg}deg)`;
+    hourHand.style.transform = `rotate(${hoursDeg}deg)`;
 }
+
 setInterval(setDate, 1000);
