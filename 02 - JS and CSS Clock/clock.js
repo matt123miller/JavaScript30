8a84e427846db510f781e50efbfcbe196507ceb1
Matt Miller <matt123miller@hotmail.co.uk>
2018-05-05 19:07:22 +0100

    Wrote some maths for manipulating the seconds hand.

--- 02 - JS and CSS Clock/clock.js
+++ 02 - JS and CSS Clock/clock.js
@@ -1,2 +1,12 @@
-console.log('clockwork');
 
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+    
+    const now = new Date();
+    const secondsDeg = (360 / 60) * now.getSeconds();
+    secondHand.style.transform = `rotate(${secondsDeg}deg)`;
+}
+setInterval(setDate, 1000);
