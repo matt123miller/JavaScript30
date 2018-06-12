0a1bd8b63778d2ed6f26edce881760c7621125e5
Matt Miller <matt123miller@hotmail.co.uk>
2018-06-12 23:02:18 +0100

    Finished CSS variables. It was good!

---
+++ 03 - CSS Variables/cssVariables.js
@@ -0,0 +1,12 @@
+
+function handleUpdate() {
+    // console.log(this.value);
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+const inputs = document.querySelectorAll('.controls input');
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
